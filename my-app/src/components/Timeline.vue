3be0901c05a679526a5f7ea03de869d837d90ebe
NHopewell <nicholashopewell@gmail.com>
2024-03-04 21:07:19 -0800

    replace initial templates with timeline

---
+++ my-app/src/components/Timeline.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const periods = ["Today", "This week", "This Month"];
+</script>
+
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs">
+            <a v-for="period of periods">
+                {{  period }}
+            </a>
+        </span>
+    </nav>
+</template>
