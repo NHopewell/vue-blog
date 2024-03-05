656192a7695bfc5b95d11fc3baee45e2489a958c
NHopewell <nicholashopewell@gmail.com>
2024-03-04 21:15:16 -0800

    add bind key

--- my-app/src/components/Timeline.vue
+++ my-app/src/components/Timeline.vue
@@ -3,11 +3,11 @@ const periods = ["Today", "This week", "This Month"];
 </script>
 
 <template>
-    <nav class="is-primary panel">
-        <span class="panel-tabs">
-            <a v-for="period of periods">
-                {{  period }}
-            </a>
-        </span>
-    </nav>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a v-for="period of periods" v-bind:key="period">
+        {{ period }}
+      </a>
+    </span>
+  </nav>
 </template>
